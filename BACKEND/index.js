0d73af5a0f5b359a407f031bd3420981a461758e
Mohsin Raza Gondal <innoxentmosu@gmail.com>
2025-02-19 22:45:26 +0500

    Worked on MAIN FEED Page

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-let port = 8080;
+let port = 2100;
 
 app.get('/home',(Request,Response)=>{
     Response.send('home Response');
 });
+
 app.listen(port, () => {
     console.log(`Server is Listening on Port ${port}`);
 });
